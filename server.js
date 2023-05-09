13c16fec672bb66ee25600d33083a7ec3354532a
SwaggyLTW <tommy22@live.unc.edu>
2023-05-09 14:16:45 -0400

    /

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ import { rps, rpsls } from './lib/rpsls.js';
 import minimist from 'minimist';
 import express from 'express';
 
-var args = minimist(process.argv.slice(2));
+var argv = minimist(process.argv.slice(2));
 var app = express();
-const port = args.port || 5000;
+const port = argv.port || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -23,28 +23,28 @@ app.get('/app/rpsls', (req, res) =>{
     res.status(200).send(rpsls());
 });
 //rps shot
-app.get('app/rps/play', (req, res)=>{
+app.get('app/rps/play/', (req, res)=>{
     res.status(200).send(rps(req.query.shot));
 });
 //rpsls shot
-app.get('app/rpsls/play', (req, res)=>{
+app.get('app/rpsls/play/', (req, res)=>{
     res.status(200).send(rpsls(req.query.shot));
 });
 //rps POST request
-app.get('app/rps/play', (req, res)=>{
+app.get('app/rps/play/', (req, res)=>{
     res.status(200).send(rps(req.body.shot));
 });
 //rpsls POST request
-app.get('app/rpsls/play', (req, res)=>{
+app.get('app/rpsls/play/', (req, res)=>{
     res.status(200).send(rpsls(req.body.shot));
 });
 //rps URL
-app.get('app/rps/play/:shot', (req, res)=>{
-    res.status(200).send(rps(req.params.arg));
+app.get('app/rps/play/:shot/', (req, res)=>{
+    res.status(200).send(rps(req.params.shot));
 });
 //rpsls URL
-app.get('app/rpsls/play/:shot', (req, res)=>{
-    res.status(200).send(rpsls(req.params.arg));
+app.get('app/rpsls/play/:shot/', (req, res)=>{
+    res.status(200).send(rpsls(req.params.shot));
 });
 //404 Not Found
 app.get('*', (req, res)=> {
